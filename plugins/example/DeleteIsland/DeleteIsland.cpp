d295cbb6a5c06a7b38844dabe3a451ded6d0fae9
Jonedydmg7562 <756272672@qq.com>
2020-11-16 00:03:17 +0800

    [changelog]添加点云去孤岛插件，需要熟悉cckdtre和ccoctre以及ccgeneric等类型才可使用cc中的数据结构进行滤波操作

--- plugins/example/DeleteIsland/DeleteIsland.cpp
+++ plugins/example/DeleteIsland/DeleteIsland.cpp
@@ -1,6 +1,6 @@
 //##########################################################################
 //#                                                                        #
-//#                CLOUDCOMPARE PLUGIN: ExamplePlugin                      #
+//#                CLOUDCOMPARE PLUGIN: DeleteIsland                      #
 //#                                                                        #
 //#  This program is free software; you can redistribute it and/or modify  #
 //#  it under the terms of the GNU General Public License as published by  #
@@ -16,11 +16,11 @@
 //##########################################################################
 
 // First:
-//	Replace all occurrences of 'ExamplePlugin' by your own plugin class name in this file.
+//	Replace all occurrences of 'DeleteIsland' by your own plugin class name in this file.
 //	This includes the resource path to info.json in the constructor.
 
 // Second:
-//	Open ExamplePlugin.qrc, change the "prefix" and the icon filename for your plugin.
+//	Open DeleteIsland.qrc, change the "prefix" and the icon filename for your plugin.
 //	Change the name of the file to <yourPluginName>.qrc
 
 // Third:
@@ -33,7 +33,7 @@
 
 #include <QtGui>
 
-#include "ExamplePlugin.h"
+#include "DeleteIsland.h"
 
 //mine_add
 #include<ccPointCloud.h>//基础点类
@@ -41,16 +41,16 @@
 // Default constructor:
 //	- pass the Qt resource path to the info.json file (from <yourPluginName>.qrc file) 
 //  - constructor should mainly be used to initialize actions and other members
-ExamplePlugin::ExamplePlugin( QObject *parent )
+DeleteIsland::DeleteIsland( QObject *parent )
 	: QObject( parent )
-	, ccStdPluginInterface( ":/CC/plugin/ExamplePlugin/info.json" )
+	, ccStdPluginInterface( ":/CC/plugin/DeleteIsland/info.json" )
 	, m_action( nullptr )
 {
 }
 
 // This method should enable or disable your plugin actions
 // depending on the currently selected entities ('selectedEntities').
-void ExamplePlugin::onNewSelection( const ccHObject::Container &selectedEntities )
+void DeleteIsland::onNewSelection( const ccHObject::Container &selectedEntities )
 {
 	if ( m_action == nullptr )
 	{
@@ -74,7 +74,7 @@ void ExamplePlugin::onNewSelection( const ccHObject::Container &selectedEntities
 
 // This method returns all the 'actions' your plugin can perform.
 // getActions() will be called only once, when plugin is loaded.
-QList<QAction *> ExamplePlugin::getActions()
+QList<QAction *> DeleteIsland::getActions()
 {
 	// default action (if it has not been already created, this is the moment to do it)
 	if ( !m_action )
@@ -86,7 +86,7 @@ QList<QAction *> ExamplePlugin::getActions()
 		m_action->setIcon( getIcon() );
 		
 		// Connect appropriate signal
-		connect( m_action, &QAction::triggered, this, &ExamplePlugin::doAction );
+		connect( m_action, &QAction::triggered, this, &DeleteIsland::doAction );
 	}
 
 	return { m_action };
@@ -97,7 +97,7 @@ QList<QAction *> ExamplePlugin::getActions()
 // main interface). You can access most of CC's components (database,
 // 3D views, console, etc.) via the 'm_app' variable (see the ccMainAppInterface
 // class in ccMainAppInterface.h).
-//void ExamplePlugin::doAction_old()
+//void DeleteIsland::doAction_old()
 //{	
 //	if ( m_app == nullptr )
 //	{
@@ -114,11 +114,11 @@ QList<QAction *> ExamplePlugin::getActions()
 //
 //	// This is how you can output messages
 //	// Display a standard message in the console
-//	m_app->dispToConsole( "[ExamplePlugin] Hello world!", ccMainAppInterface::STD_CONSOLE_MESSAGE );
-//	m_app->dispToConsole("[ExamplePlugin] Hello Aijun Shi!", ccMainAppInterface::STD_CONSOLE_MESSAGE);
+//	m_app->dispToConsole( "[DeleteIsland] Hello world!", ccMainAppInterface::STD_CONSOLE_MESSAGE );
+//	m_app->dispToConsole("[DeleteIsland] Hello Aijun Shi!", ccMainAppInterface::STD_CONSOLE_MESSAGE);
 //
 //	// Display a warning message in the console
-//	m_app->dispToConsole( "[ExamplePlugin] Warning: example plugin shouldn't be used as is", ccMainAppInterface::WRN_CONSOLE_MESSAGE );
+//	m_app->dispToConsole( "[DeleteIsland] Warning: example plugin shouldn't be used as is", ccMainAppInterface::WRN_CONSOLE_MESSAGE );
 //	
 //	// Display an error message in the console AND pop-up an error box
 //	m_app->dispToConsole( "Example plugin shouldn't be used - it doesn't do anything!", ccMainAppInterface::ERR_CONSOLE_MESSAGE );
@@ -127,7 +127,7 @@ QList<QAction *> ExamplePlugin::getActions()
 //}
 
 
-void ExamplePlugin::doAction()
+void DeleteIsland::doAction()
 {
 	if (m_app == nullptr)
 	{
@@ -157,11 +157,21 @@ void ExamplePlugin::doAction()
 	}
 	// 插件功能：把选择的点云的x坐标增加100
 	ccPointCloud* pc = static_cast<ccPointCloud*>(ent);
+
+	//复制点云
 	ccPointCloud* pc_new = new ccPointCloud("new cc");
+
 	for (int i = 0; i < pc->size(); ++i)
 	{
-		pc_new->addPoint(CCVector3(pc->getPoint(i)->x + 10, pc->getPoint(i)->y, pc->getPoint(i)->z));
+		pc_new->addPoint(CCVector3(pc->getPoint(i)->x + 30, pc->getPoint(i)->y, pc->getPoint(i)->z));
 	}
+
+	//需要将点云转换为generic格式，才可调用数据结构
+	//ccGenericPointCloud gpc(pc_new);
+
+	//需要利用kd树（cckdtree）或者八叉树（ccoctree）进行邻域搜索，达到邻域聚类滤波的目的
+
+
 
 	ccHObject* cc_containers = new ccHObject("new-cc");
 	cc_containers->addChild(pc_new);
@@ -171,10 +181,10 @@ void ExamplePlugin::doAction()
 
 	// This is how you can output messages
 	// Display a standard message in the console
-	m_app->dispToConsole("[ExamplePlugin] plugin run successfully!", ccMainAppInterface::STD_CONSOLE_MESSAGE);
+	m_app->dispToConsole("[DeleteIsland] plugin run successfully!", ccMainAppInterface::STD_CONSOLE_MESSAGE);
 
 	//Display a warning message in the console
-		//m_app->dispToConsole( "[ExamplePlugin] Warning: example plugin shouldn't be used as is", ccMainAppInterface::WRN_CONSOLE_MESSAGE );
+		//m_app->dispToConsole( "[DeleteIsland] Warning: example plugin shouldn't be used as is", ccMainAppInterface::WRN_CONSOLE_MESSAGE );
 		//
 		//Display an error message in the console AND pop - up an error box
 		//m_app->dispToConsole( "Example plugin shouldn't be used - it doesn't do anything!", ccMainAppInterface::ERR_CONSOLE_MESSAGE );
